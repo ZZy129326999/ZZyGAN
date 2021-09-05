ddfddccccfb00b76aeef5b8a4403d60301ac05f3
ZZy129326999 <1725617470@qq.com>
2021-09-05 01:28:48 +0000

    add ./

--- MusicFeatures/timmlstm.py
+++
@@ -1,8 +0,0 @@
-import torch 
-
-path = r"GANss/MusicFeatures/pytorch-image-models/output/train/20210725-052720-resnet50-224/model_best.pth.tar"
-data_path = r'./data/images_general/'
-
-model = torch.load(path)
-
-
