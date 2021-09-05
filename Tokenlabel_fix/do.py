54319a058d3610431a88b4b475fa119f14e16d6c
ZZy129326999 <1725617470@qq.com>
2021-09-05 02:45:16 +0000

    ZZy

---
+++ Tokenlabel_fix/do.py
@@ -0,0 +1,12 @@
+import os 
+from PIL import Image 
+path = r'../volo/RiceDiseases/'
+save_path = r'./RiceDiseases/'
+# file = '.ipynb_checkpoints'
+# for root, dirs, _ in os.walk(path):
+#     for subdir in dirs:
+#         path_ = os.path.join(root, subdir)
+#         print(path_)
+#         if file in path_:
+#             os.removedirs(path_)
+
